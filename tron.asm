190e4f873fce2785e77e15bb8bea660d067bb439
infopek <10saero01@gmail.com>
2023-10-18 12:12:24 +0200

    Tidy up, finish main menu

--- tron.asm
+++ tron.asm
@@ -4,29 +4,29 @@ stck ends
 
 data segment para 'data'
 	; main menu
-	curr_option         db ?                                                                  	; current option selected by user
-	chosen_option       db ?                                                                  	; chosen option by user
+	curr_option         db ?                                                                             	; current option selected by user
+	chosen_option       db ?                                                                             	; chosen option by user
 	has_chosen_flag     db 0
 
 	; window
 	max_width           dw 320
 	max_height          dw 200
 
-	border_x            dw ?                                                                  	; helper var to draw border (x pos of pointer)
-	border_y            dw ?                                                                  	; helper var to draw border (y pos of pointer)
+	border_x            dw ?                                                                             	; helper var to draw border (x pos of pointer)
+	border_y            dw ?                                                                             	; helper var to draw border (y pos of pointer)
 
 	; player
-	p1_x                dw ?                                                                  	; x position of player 1
-	p1_y                dw ?                                                                  	; y position of player 1
+	p1_x                dw ?                                                                             	; x position of player 1
+	p1_y                dw ?                                                                             	; y position of player 1
 
-	v1_x                dw ?                                                                  	; x velocity of player 1
-	v1_y                dw ?                                                                  	; y velocity of player 1
+	v1_x                dw ?                                                                             	; x velocity of player 1
+	v1_y                dw ?                                                                             	; y velocity of player 1
 
-	p2_x                dw ?                                                                  	; x position of player 2
-	p2_y                dw ?                                                                  	; y position of player 2
+	p2_x                dw ?                                                                             	; x position of player 2
+	p2_y                dw ?                                                                             	; y position of player 2
 
-	v2_x                dw ?                                                                  	; x velocity of player 2
-	v2_y                dw ?                                                                  	; y velocity of player 2
+	v2_x                dw ?                                                                             	; x velocity of player 2
+	v2_y                dw ?                                                                             	; y velocity of player 2
 
 	; default values for variables
 	curr_option_def     db 0
@@ -52,20 +52,20 @@ data segment para 'data'
 	v2_y_def            dw 0
 
 	; player scores
-	p1_score            db 0                                                                  	; # of wins for player1
-	p2_score            db 0                                                                  	; # of wins for player2
+	p1_score            db 0                                                                             	; # of wins for player1
+	p2_score            db 0                                                                             	; # of wins for player2
 
 	; game state
-	prev_time           db 0                                                                  	; to check if time has elapsed
-	prev_time_countdown db 0                                                                  	; to check if time has elapsed
+	prev_time           db 0                                                                             	; to check if time has elapsed
+	prev_time_countdown db 0                                                                             	; to check if time has elapsed
 
 	countdown_secs      db ?
 
 	p1_won_flag         db 0
 	p2_won_flag         db 0
 
-	is_gameover_flag    db 0                                                                  	; flag to check if game is over
-	restart_flag        db 0                                                                  	; flag to check is players want to play again
+	is_gameover_flag    db 0                                                                             	; flag to check if game is over
+	restart_flag        db 0                                                                             	; flag to check is players want to play again
 
 	; colors (from VGA 256-color palette)
 	red                 db 28h
@@ -90,7 +90,10 @@ data segment para 'data'
 	pointer_symbol      db ">$"
 	empty_space         db " $"
 
-	selection_help_text db "Press [T] to cycle up, [G] to cycle down, [K] to choose selected$"
+	selection_help_text db "Press [T] to cycle options, [C] to choose selected$"
+	player1_controls    db "[W] - Up", 13, 10, "[A] - Left", 13, 10, "[S] - Down", 13, 10, "[D] - Right$"
+	player2_controls    db "[I] - Up", 13, 10, "[J] - Left", 13, 10, "[K] - Down", 13, 10, "[L] - Right$"
+	back_text           db "Press [B] to go back to main menu$"
 
 	player1_wins_text   db "Player 1 wins!$"
 	player2_wins_text   db "Player 2 wins!$"
@@ -117,10 +120,10 @@ main proc far
 
 	                      pop    ax
 
+	; main menu
 	                      call   clear_screen
 
 	                      call   main_menu                 	; display main menu
-	                      call   evaluate_option           	; decide where to go with selected option
 
 	game_setup:           
 	                      call   clear_screen
@@ -141,8 +144,11 @@ main proc far
 	                      int    10h
 
 	                      call   calc_start_pos            	; from video mode resolution
+
 	                      call   reset_game_state
+
 	                      call   set_colors
+
 	                      call   draw_border
 	gameloop:             
 	; handle input
@@ -193,10 +199,58 @@ main proc far
 main endp
 
 evaluate_option proc near
+	                      cmp    chosen_option, 0          	; option 0: play game
+	                      je     play_game
 
+	                      cmp    chosen_option, 1          	; option 1: show controls
+	                      je     display_controls
 
 	                      ret
+
+	display_controls:     
+	                      call   show_player_controls
+
+	                      mov    ah, 00h
+	                      int    16h
+
+	                      cmp    al, 'b'                   	; back to main menu?
+	                      jz     back_to_menu
+
+	                      jmp    display_controls
+
+	back_to_menu:         
+	                      call   clear_screen
+	                      mov    has_chosen_flag, 0        	; reset chosen flag
+	                      call   main_menu
+	                      ret
+
+	play_game:            
+	                      ret
 evaluate_option endp
+
+show_player_controls proc near
+	                      call   clear_screen
+
+	; player1 controls
+	                      push   0
+	                      push   4
+	                      push   offset player1_controls
+	                      call   print_string_at
+
+	; player2 controls
+	                      push   0
+	                      push   10
+	                      push   offset player2_controls
+	                      call   print_string_at
+
+	; back to main menu text
+	                      push   30
+	                      push   15
+	                      push   offset back_text
+	                      call   print_string_at
+
+	                      ret
+show_player_controls endp
 
 	; display main menu and polls options, exists when one is chosen
 main_menu proc near
@@ -204,20 +258,20 @@ main_menu proc near
 	; so that two pointer arrows can be displayed cleanly
 	                      push   25
 	                      push   4
-	                      call   move_cursor
-
-	; print welcome text
 	                      push   offset welcome_text
-	                      call   print_string
+	                      call   print_string_at
 
 	                      call   display_options
 
 	                      call   display_options_guide     	; help for the user
 
-	; init default chosen option
+	; init defaults
+	                      mov    ah, curr_option_def
+	                      mov    curr_option, ah
+	                      call   move_ptr
+
 	                      mov    ah, chosen_option_def
 	                      mov    chosen_option, ah
-	                      call   display_option
 
 	; loop until an option is chosen
 	poll_options:         
@@ -229,7 +283,7 @@ main_menu proc near
 	                      jmp    poll_options              	; not yet
 
 	exit_main_menu:       
-	                      mov    has_chosen_flag, 0
+	                      call   evaluate_option
 	                      ret
 main_menu endp
 
@@ -238,10 +292,8 @@ display_options proc near
 	; option 1: play game
 	                      push   33
 	                      push   8
-	                      call   move_cursor
-
 	                      push   offset play_game_text
-	                      call   print_string
+	                      call   print_string_at
 
 	; option 2: show player controls
 	                      push   31
@@ -255,12 +307,10 @@ display_options proc near
 display_options endp
 
 display_options_guide proc near
-	                      push   10
 	                      push   15
-	                      call   move_cursor
-
+	                      push   15
 	                      push   offset selection_help_text
-	                      call   print_string
+	                      call   print_string_at
 
 	                      ret
 display_options_guide endp
@@ -268,56 +318,50 @@ display_options_guide endp
 	; prompts the user to choose an option in main menu
 cycle_options proc near
 	choose_option:        
-	                      mov    ah, 01h
-	                      int    16
-	                      jz     no_choice
-
 	                      mov    ah, 00h
 	                      int    16h
 	                      jmp    handle_choice
 
-	no_choice:            
-	                      ret
-
 	handle_choice:        
 	                      cmp    al, 't'
-	                      je     key_is_t
+	                      je     change_selected1
 
-	                      cmp    al, 'g'
-	                      je     key_is_g
-
-	                      cmp    al, 'k'
+	                      cmp    al, 'c'
 	                      je     select_option
 
 	                      ret
 
-	key_is_t:             
+	change_selected1:     
 	                      cmp    curr_option, 0            	; check if current selected option is the first one
-	                      je     key_is_g
-
-	                      call   display_option
+	                      je     change_selected2
 
 	                      mov    curr_option, 0            	; make current selected option the first one
 
+	                      call   move_ptr
+
 	                      ret
 
-	key_is_g:             
+	change_selected2:     
 	                      cmp    curr_option, 1            	; check if current selected option is the second one
-	                      je     key_is_t
-
-	                      call   display_option
+	                      je     change_selected1
 
 	                      mov    curr_option, 1            	; make current selected option the second one
+
+	                      call   move_ptr
 
 	                      ret
 
 	select_option:        
 	                      mov    has_chosen_flag, 1
 
+	; current selected is the chosen option
+	                      mov    ah, curr_option
+	                      mov    chosen_option, ah
+
 	                      ret
 cycle_options endp
 
-display_option proc near
+move_ptr proc near
 	; check which option is selected
 	                      cmp    curr_option, 0
 	                      je     move_ptr_up
@@ -331,17 +375,13 @@ display_option proc near
 	; erase previous arrow
 	                      push   28
 	                      push   10
-	                      call   move_cursor
-
 	                      push   offset empty_space
-	                      call   print_string
+	                      call   print_string_at
 	; print current
 	                      push   28
 	                      push   8
-	                      call   move_cursor
-
 	                      push   offset pointer_symbol
-	                      call   print_string
+	                      call   print_string_at
 
 	                      ret
 
@@ -349,20 +389,16 @@ display_option proc near
 	; erase previous arrow
 	                      push   28
 	                      push   8
-	                      call   move_cursor
-
 	                      push   offset empty_space
-	                      call   print_string
+	                      call   print_string_at
 
 	                      push   28
 	                      push   10
-	                      call   move_cursor
-
 	                      push   offset pointer_symbol     	; print little arrow
-	                      call   print_string
+	                      call   print_string_at
 
 	                      ret
-display_option endp
+move_ptr endp
 
 countdown proc near
 	second_loop:          
@@ -493,10 +529,8 @@ reset_game_state endp
 ask_restart proc near
 	                      push   33
 	                      push   14
-	                      call   move_cursor
-
 	                      push   offset again_text
-	                      call   print_string
+	                      call   print_string_at
 
 	ask_restart_input:    
 	                      push   47
@@ -530,10 +564,8 @@ ask_restart proc near
 	no_restart:           
 	                      push   30
 	                      push   17
-	                      call   move_cursor
-
 	                      push   offset gameover_text
-	                      call   print_string
+	                      call   print_string_at
 
 	                      ret
 ask_restart endp
@@ -739,6 +771,7 @@ move_player2 proc near
 	                      add    p2_y, ax                  	; new_y = old_y + vel_y
 
 	                      call   check_p2_collisions
+
 	                      ret
 move_player2 endp
 
@@ -776,6 +809,7 @@ check_p1_collisions proc near
 
 	                      mov    ah, p1_trail_color
 	                      push   ax
+
 	                      call   check_collision
 
 	                      ret
@@ -815,6 +849,7 @@ check_p2_collisions proc near
 
 	                      mov    ah, p1_trail_color
 	                      push   ax
+
 	                      call   check_collision
 
 	                      ret
@@ -847,6 +882,7 @@ check_collision endp
 
 evaluate_game_state proc near
 	                      mov    is_gameover_flag, 1       	; set it to true originally
+						  
 	; states: nothing, p1 won, p2 won, tie
 	                      mov    ah, p1_won_flag
 	                      and    ah, p2_won_flag           	; tie
@@ -863,7 +899,7 @@ evaluate_game_state proc near
 
 	tie:                  
 	; display tie text, no scores affected
-	                      call   change_to_text_mode
+	                      call   move_cursor_to_center
 
 	                      push   offset tie_text
 	                      call   print_string
@@ -871,7 +907,7 @@ evaluate_game_state proc near
 	                      ret
 
 	player1_won:          
-	                      call   change_to_text_mode
+	                      call   move_cursor_to_center
 
 	                      push   offset player1_wins_text
 	                      call   print_string
@@ -881,7 +917,7 @@ evaluate_game_state proc near
 	                      ret
 
 	player2_won:          
-	                      call   change_to_text_mode
+	                      call   move_cursor_to_center
 
 	                      push   offset player2_wins_text
 	                      call   print_string
@@ -892,7 +928,7 @@ evaluate_game_state proc near
 evaluate_game_state endp
 
 	; changes to text mode and move cursor to center of console
-change_to_text_mode proc near
+move_cursor_to_center proc near
 	; change to text mode
 	                      mov    ax, 03h
 	                      int    10h
@@ -902,7 +938,7 @@ change_to_text_mode proc near
 	                      call   move_cursor
 
 	                      ret
-change_to_text_mode endp
+move_cursor_to_center endp
 
 draw_border proc near
 	; (0, 0) -> (max_width, 0)
@@ -1019,15 +1055,15 @@ move_cursor proc near
 	                      ret    4
 move_cursor endp
 
-	; gets pixel color at (row = arg2, col = arg1), puts result in 'al' register
+	; gets pixel color at (arg1 = row, arg2 = col), puts result in 'al' register
 get_pixel_color proc near
 	                      push   bp
 	                      mov    bp, sp
 
 	                      mov    ah, 0dh                   	; set config to get pixel
 	                      mov    bh, 0                     	; page number
-	                      mov    cx, [[bp + 4]]            	; row
-	                      mov    dx, [[bp + 6]]            	; col
+	                      mov    cx, [[bp + 4]]            	; col
+	                      mov    dx, [[bp + 6]]            	; row
 	                      int    10h                       	; al = result
 
 	                      pop    bp
@@ -1044,11 +1080,12 @@ draw_border_pixel proc near
 	                      ret
 draw_border_pixel endp
 
+	; arg1 = row, arg2 = col, arg3 = color
 draw_pixel proc near
 	                      push   bp
 	                      mov    bp, sp
 
-	                      mov    bx, [bp + 4]
+	                      mov    bx, [bp + 4]              	; color
 
 	                      mov    ah, 0ch                   	; set config to draw pixel
 	                      mov    al, bh                    	; color is in upper byte
@@ -1073,6 +1110,22 @@ print_string proc near
 	                      pop    bp
 	                      ret    2
 print_string endp
+
+	; arg1 = col, arg2 = row, arg3 = string to print
+print_string_at proc near
+	                      push   bp
+	                      mov    bp, sp
+
+	                      push   [bp + 8]
+	                      push   [bp + 6]
+	                      call   move_cursor
+
+	                      push   [bp + 4]
+	                      call   print_string
+
+	                      pop    bp
+	                      ret    6
+print_string_at endp
 
 clear_screen proc near
 	                      mov    ax, 03h
